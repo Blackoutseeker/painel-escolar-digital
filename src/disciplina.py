d8f2e08aaff63ce3478885cf85f61082d75add0d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-12-14 05:30:32 -0300

    style(src): format file using PEP 8 Style Guide

--- src/disciplina.py
+++ src/disciplina.py
@@ -1,17 +1,15 @@
-class disciplina:
+class Disciplina:
+    def __init__(self, nome, codigo):
+        self.nome = nome
+        self.codigo = codigo
+        self.notas = {}
 
-    def __init__(self,nome,codigo):
-    self.nome = nome
-    self.codigo = codigo
-    # dicionario:aluno -> lista de notas
-    self.notas = {} 
-    
     def adicionar_notas(self, aluno, nota):
         if aluno not in self.notas:
             self.notas[aluno] = []
-        self.notas[aluno].append (nota)
+        self.notas[aluno].append(nota)
         
     def media_aluno(self, aluno):
         if aluno in self.notas:
             return sum(self.notas[aluno]) / len(self.notas[aluno])
-        return None
+        return 0
