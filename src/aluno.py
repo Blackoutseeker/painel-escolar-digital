72fa4a40a9c3906570e60daa274208663c3c7c2a
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-12-15 19:43:18 -0300

    refactor(src): override get_tipo method from Aluno class

--- src/aluno.py
+++ src/aluno.py
@@ -2,9 +2,12 @@ from pessoa import Pessoa
 
 
 class Aluno(Pessoa):
-    def __init__(self, nome, idade, matricula, id_turma):
-        super().__init__(nome, idade)
-        self.matricula = matricula
-        self.id_turma = id_turma
-        self.notas = []
-        self.frequencias = []
+    def __init__(self, nome, cpf, data_nascimento, matricula, id_turma):
+        super().__init__(nome, cpf, data_nascimento)
+        self.__matricula = matricula
+        self.__id_turma = id_turma
+        self.__notas = []
+        self.__frequencias = []
+
+    def get_tipo(self):
+        return 'ALUNO'
